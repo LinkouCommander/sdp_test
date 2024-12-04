26fa85922d96af338c09a5139827b2412f9a47e0
Brian <chsieh06@usc.edu>
2024-12-04 14:35:58 -0800

    test

--- sdp_serial.cpp
+++ sdp_serial.cpp
@@ -149,6 +149,11 @@ void scaled_dot_product_attention(float**** query, float**** key, float**** valu
 
     matrix_multiply(attn_weight, value, output, batch_size, num_heads, L, D, S);
 
+    cout << &output[63][11][63][62] << " " << &output[63][11][63][63] << "\n";
+    cout << &output[63][11][62][63] << " " << &output[63][11][63][0] << "\n";
+    cout << &output[63][10][63][63] << " " << &output[63][11][0][0] << "\n";
+    cout << &output[62][11][63][63] << " " << &output[63][0][0][0] << "\n";
+    
     for(int i = 0; i < 8; i++) {
         for(int j = 0; j < 8; j++) {
             cout << output[5][5][i][j] << " ";
