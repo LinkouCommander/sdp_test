2bfb090dbbfe72825c7de8f0d069f28b571ac9ed
Brian <chsieh06@usc.edu>
2024-12-10 21:06:49 -0800

    test

--- sdp_bmm.cpp
+++ sdp_bmm.cpp
@@ -192,8 +192,8 @@ int main(int argc, char *argv[]){
 
     int batch_size = 64;
     int num_heads = 12;
-    int L = 64;
-    int S = 64;
+    int L = 256;
+    int S = 256;
     int D = 1024;
 
     float**** query = new float***[batch_size];
